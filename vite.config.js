7c0c2e2818c8e66fd16ea2ade84d0460617cedfc
Almas <aegeubai@bk.ru>
2025-09-22 14:51:53 +0600

    Обновлен проект

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://host.docker.internal:6969', // вот сюда
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '/api')
-      }
-    }
-  }
+  base: '/', 
 })
